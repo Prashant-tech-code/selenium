66fc55dc0163d5dcca8ec6782611187af26d565c
LENOVO <LENOVO@DESKTOP-L4VD9FV>
2025-11-09 10:38:28 +0530

    selenium 1st commit

---
+++ mavenpractice/src/main/java/prashant/mavenpractice/App.java
@@ -0,0 +1,10 @@
+package prashant.mavenpractice;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ mavenpractice/src/test/java/prashant/mavenpractice/Google.java
@@ -0,0 +1,20 @@
+package prashant.mavenpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+
+	}
+
+}
